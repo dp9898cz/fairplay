23b4ac47e54b8a760ae58361319a8c1d3abbf057
Daniel <xpatek08@vutbr.cz>
2022-10-14 20:56:52 +0200

    debugged

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import unicodedata
+import sys  # arguments
 from collections import OrderedDict
+
 
 abeceda = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 cisla = '0123456789'
@@ -17,6 +19,8 @@ def filtraceKlice(klic):
     klic = ''.join(znak if znak in abeceda else '' for znak in klic)
     # odstranění duplicit
     klic = "".join(OrderedDict.fromkeys(klic))
+    # j==i
+    klic = klic.replace("J", "I")
 
     return klic
 
@@ -31,12 +35,13 @@ def filtraceTextu(text):
     text = text.upper()
     # odstranění dalších znaků
     text = ''.join(znak if znak in abeceda else '' for znak in text)
+    # j==i
+    text = text.replace("J", "I")
 
     return text
 
 
-def vytvoritTabulku(klic):
-    klic = filteredKey
+""" def vytvoritTabulku(klic):
     matrix = [[0 for i in range(5)] for j in range(5)]
     letters_added = []
     row = 0
@@ -56,21 +61,39 @@ def vytvoritTabulku(klic):
     # Add the rest of the alphabet to the matrix
     # A=65 ... Z=90
     for letter in range(65, 91):
-        if letter == 74:  # I/J are in the same position
+        if letter == 74:  # I(74)/J(75) are in the same position
             continue
         if chr(letter) not in letters_added:  # Do not add repeated letters
             letters_added.append(chr(letter))
 
-   # print (len(letters_added), letters_added)
+    # print (len(letters_added), letters_added)
     index = 0
     for i in range(5):
         for j in range(5):
             matrix[i][j] = letters_added[index]
             index += 1
+    return matrix """
+
+
+def create_fill_matrix(characters):
+    matrix = [[0 for i in range(5)] for j in range(5)]
+    index = 0
+    for i in range(5):
+        for j in range(5):
+            matrix[i][j] = characters[index]
+            index += 1
     return matrix
 
 
-def separate_same_letters(text):
+def make_matrix(key):
+    characters = abeceda.replace('J', '')  # j==i
+    index = 0
+    for char in key:
+        characters = characters.replace(char, "")
+    return create_fill_matrix(key + characters)
+
+
+""" def separate_same_letters(text):
     index = 0
     while (index < len(text)):
         l1 = text[index]
@@ -82,6 +105,34 @@ def separate_same_letters(text):
         if l1 == l2:
             text = text[:index+1] + "X" + text[index+1:]
         index += 2
+    return text """
+
+
+def get_avaiable_separator(text):
+    for char in abeceda:
+        if not char in text:
+            # choose character not used in text as separator
+            return char
+    return 'X'  # default
+
+
+def separate_letters(text, separator):
+    resolved = False
+    while (not resolved) and len(text) != 0:
+        for idx, char in enumerate(text):
+            if (idx > 0 and char == text[idx - 1]):
+                # found two characters next to each other
+                text = text[:idx] + separator + text[idx:]
+                break
+            if (idx == len(text) - 1):
+                # reached the end, we can safely mark this string as resolved
+                resolved = True
+    return text
+
+
+def check_even_length(text, separator):
+    if len(text) % 2 == 1:
+        return text + separator
     return text
 
 
@@ -109,7 +160,7 @@ def sifrovat(text, matrix):
     return " ".join(cipher_text[i:i + 5] for i in range(0, len(cipher_text), 5))
 
 
-def desifrovat(text, matrix):
+def desifrovat(text, matrix, separator):
     text = text.replace(" ", "")
     cipher_text = ''
     for (l1, l2) in zip(text[0::2], text[1::2]):
@@ -122,11 +173,16 @@ def desifrovat(text, matrix):
         else:  # Rule 4, the letters are in a different row and column
             cipher_text += matrix[row1][col2] + matrix[row2][col1]
     cipher_text = cipher_text.replace("XSPACEX", " ")
+    cipher_text = cipher_text.replace(separator, "")
     return cipher_text
 
 
 if __name__ == "__main__":
     klic = None
+    text = None
+    if (len(sys.argv) == 3):
+        klic = filtraceKlice(sys.argv[1])
+        text = filtraceTextu(sys.argv[2])
     while (klic == None):
         filteredKey = filtraceKlice(input("Zadej klíč: "))
         if len(filteredKey) < 4:
@@ -134,17 +190,20 @@ if __name__ == "__main__":
         else:
             klic = filteredKey
 
-    matrix = vytvoritTabulku(klic)
+    matrix = make_matrix(klic)
     print(matrix)
 
-    text = filtraceTextu(input("Zadej text pro šifrování: "))
+    if (not text):
+        text = filtraceTextu(input("Zadej text pro šifrování: "))
     print("Zadaný text po filtraci: " + text)
 
-    text = separate_same_letters(text)
+    separator = get_avaiable_separator(text)
+    text = separate_letters(text, separator)
+    text = check_even_length(text, separator)
     print("Zadaný text po rozdeleni stejnych pismen: " + text)
 
     cipher = sifrovat(text, matrix)
     print("Šifrovaný text: " + cipher)
 
-    decipher = desifrovat(cipher, matrix)
+    decipher = desifrovat(cipher, matrix, separator)
     print("Znovu dešifrovaný text: " + decipher)
